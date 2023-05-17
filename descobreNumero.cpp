e99a03d577afa6dbf8f78f8ac993d81a626f130d
Alexandre Dantas <102476384+AlexandreDantasz@users.noreply.github.com>
2023-05-16 22:51:13 -0300

    .

--- descobreNumero.cpp
+++ descobreNumero.cpp
@@ -1,3 +1,4 @@
+// Autor: Alexandre Dantas de Mendon√ßa
 #include <iostream>
 #include <math.h>
 #include <locale.h>
@@ -12,17 +13,17 @@ int where(int *p, int low, int high)
 	int mid = (low + high) / 2;
 	if(low == high)
 	{
-		printf("seu n˙mero È %i!\n", p[mid]);
+		printf("seu n√∫mero √© %i!\n", p[mid]);
 		return questions;
 	}
 	questions++;
-	printf("seu n˙mero È maior, menor ou igual a %i?\nR: ", p[mid]);
+	printf("seu n√∫mero √© maior, menor ou igual a %i?\nR: ", p[mid]);
 	cin >> answer;
 	for(int i = 0; i < answer.size(); i++) answer[i] = tolower(answer[i]);
 	while(answer != "igual" and answer != "maior" and answer != "menor")
 	{
 		puts("A resposta foi escrita de maneira incorreta, tente novamente.");
-		printf("seu n˙mero È maior, menor ou igual a %i?\nR: ", p[mid]);
+		printf("seu n√∫mero √© maior, menor ou igual a %i?\nR: ", p[mid]);
 		cin >> answer;
 		for(int i = 0; i < answer.size(); i++) answer[i] = tolower(answer[i]);
 	}
@@ -38,14 +39,14 @@ int where(int *p, int low, int high)
 int main()
 {
 	setlocale(LC_ALL, "Portuguese");
-	printf("seu n˙mero inteiro est· entre 1 e ");
+	printf("seu n√∫mero inteiro est√° entre 1 e ");
 	int size, maxOfQuestions;
 	cin >> size;
 	maxOfQuestions = (int)round(log(size)/log(2));
-	printf("tenho que descobrir seu n˙mero inteiro com no m·ximo %i perguntas!\n", maxOfQuestions);
+	printf("tenho que descobrir seu n√∫mero inteiro com no m√°ximo %i perguntas!\n", maxOfQuestions);
 	int vet[size];
 	for(int i = 1; i <= size; i++) vet[i - 1] = i;
 	int numberOfQuestions = where(vet, 0, size - 1);
-	printf("n˙mero de perguntas feitas: %i\n", numberOfQuestions);
+	printf("n√∫mero de perguntas feitas: %i\n", numberOfQuestions);
 	return 0;
 }
